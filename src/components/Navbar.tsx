61f834efb73353c3ff05ab6cb1fc681c9a4eed58
Paul Faass <paul.faass@pxio.de>
2023-02-02 11:34:04 +0100

    feat(ui): :sparkles: add navItem component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,24 @@
 // import Image from "next/image";
 import Link from "next/link";
+import { Landmark, LayoutDashboard } from "lucide-react";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
+  const router = useRouter();
+
+  const navItems: NavItemProps[] = [
+    {
+      name: "Dashboard",
+      icon: <LayoutDashboard />,
+      href: "/dashboard",
+    },
+    {
+      name: "Schools",
+      icon: <Landmark />,
+      href: "/schools",
+    },
+  ];
+
   return (
     <>
       <button
@@ -47,28 +64,42 @@ export default function Navbar() {
               Flowbite
             </span>
           </a> */}
-          <ul className="space-y-2">
-            <li>
-              <Link
-                href="/dashboard"
-                className="flex items-center rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-              >
-                <svg
-                  aria-hidden="true"
-                  className="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
-                  <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
-                </svg>
-                <span className="ml-3">Dashboard</span>
-              </Link>
-            </li>
-          </ul>
+          <div className="space-y-2">
+            {navItems.map((item) => (
+              <NavItem
+                key={item.name}
+                {...item}
+                active={router.pathname === item.href}
+              />
+            ))}
+          </div>
         </div>
       </aside>
     </>
   );
 }
+
+type NavItemProps = {
+  name: string;
+  href: string;
+  active?: boolean;
+  icon?: React.ReactNode;
+};
+
+const NavItem = ({ name, href, active = false, icon }: NavItemProps) => {
+  const activeClass = active && "bg-gray-200 text-gray-900 dark:bg-gray-700";
+  return (
+    <Link
+      href={href}
+      className={[
+        "flex items-center rounded-lg p-2 text-base font-normal text-gray-700 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700",
+        activeClass,
+      ].join(" ")}
+    >
+      <span className="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-800 dark:text-gray-400 dark:group-hover:text-white">
+        {icon}
+      </span>
+      <span className="ml-3">{name}</span>
+    </Link>
+  );
+};
