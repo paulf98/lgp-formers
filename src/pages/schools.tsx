56ce1497895317c600e11a116437b9881ac8583e
Paul Faass <paul.faass@pxio.de>
2023-02-02 12:19:51 +0100

    feat(ui): :sparkles: make add school form prettier and display existing schools in a table

--- src/pages/schools.tsx
+++ src/pages/schools.tsx
@@ -1,19 +1,14 @@
 import type { NextPage } from "next";
-import { useSession } from "next-auth/react";
 import { useState } from "react";
+import Button from "../components/Button";
 import { api } from "../utils/api";
 
 const Schools: NextPage = () => {
-  const session = useSession();
-
   const [school, setSchool] = useState({
     name: "",
     location: "",
   });
 
-  const user = api.user.findUser.useQuery({
-    userId: session.data?.user?.id || "",
-  });
   const schools = api.school.list.useQuery();
   const createSchoolMutation = api.school.createSchool.useMutation({
     onSuccess: () => {
@@ -26,34 +21,20 @@ const Schools: NextPage = () => {
     },
   });
 
-  const addSchoolToUserMutation = api.user.addSchool.useMutation({
-    onSuccess: () => {
-      void schools.refetch();
-    },
-  });
-
   const deleteSchool = (id: string) => {
     deleteSchoolMutation.mutate({
       id,
     });
   };
 
-  const addSchoolToUser = () => {
-    addSchoolToUserMutation.mutate({
-      userId: user.data?.id || "",
-      schoolId: schools?.data?.[0]?.id || "",
-    });
-  };
-
-  const addNewSchool = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
+  const addNewSchool = () => {
     createSchoolMutation.mutate(school);
   };
 
   return (
     <div>
-      <form onSubmit={addNewSchool}>
+      <div className="mx-auto my-8 flex max-w-sm flex-col gap-2">
+        <h1 className="mb-2 text-2xl font-bold">Add new School</h1>
         <input
           type="text"
           name="name"
@@ -68,18 +49,50 @@ const Schools: NextPage = () => {
           value={school.location}
           onChange={(e) => setSchool({ ...school, location: e.target.value })}
         />
-        <input type="submit" value="Submit" />
-      </form>
-      {schools.data?.map((school) => (
-        <div key={school.id}>
-          <p>{school.name}</p>
-          <button onClick={() => deleteSchool(school.id)}>Delete Entry</button>
-        </div>
-      ))}
-      <>{user.data?.name}</>
-      <button onClick={() => addSchoolToUser()}>
-        Add School to current User
-      </button>
+        <Button label="Submit" onClick={addNewSchool} />
+      </div>
+
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
+          <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                School name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Location
+              </th>
+              <th scope="col" className="px-6 py-3">
+                <span className="sr-only">Delete</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {schools.data?.map((school) => (
+              <tr
+                key={school.id}
+                className="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600"
+              >
+                <th
+                  scope="row"
+                  className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white"
+                >
+                  {school.name}
+                </th>
+                <td className="px-6 py-4">{school.location}</td>
+                <td className="px-6 py-4 text-right">
+                  <button
+                    onClick={() => deleteSchool(school.id)}
+                    className="font-medium text-red-600 hover:underline dark:text-red-500"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
