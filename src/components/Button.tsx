3bdee26ea14dab611707c0e6b878448ad35f31ac
Paul Faass <paul.faass@pxio.de>
2023-02-02 12:19:07 +0100

    feat(ui): :sparkles: add button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+type ButtonProps = {
+  label: string;
+  onClick: () => void;
+  type?: "button" | "submit" | "reset";
+};
+
+export default function Button({
+  label,
+  onClick,
+  type = "button",
+}: ButtonProps) {
+  return (
+    <button
+      type={type}
+      className="rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+}
